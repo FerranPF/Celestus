12bd2f0904c976763a23629dd7ca2362587ee41a
Ferran Pérez <ferranpf98@gmail.com>
2018-10-08 16:52:14 +0200

    Rotación guiada por el movimiento

--- Assets/Prototipo/Scripts/PlayerController.cs
+++ Assets/Prototipo/Scripts/PlayerController.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	
+	[SerializeField]
+	float movementSpeed = 0.0f;
+	void Update(){
+		ControlPlayer();
+	}
+	void ControlPlayer(){
+		float moveHorizonat = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
 
+		Vector3 movement = new Vector3(moveHorizonat, 0.0f, moveVertical);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+		if(movement != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
+		transform.Translate (movement*movementSpeed*Time.deltaTime, Space.World);
+	}
+
+/*
 	public float moveSpeed;
 	private Camera mainCamera;
 	private Rigidbody rb;
@@ -19,9 +35,13 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
 		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 		moveVelocity = moveInput * moveSpeed;
- 
+
+		
+
+		
 		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
 		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
 		float rayLength;
@@ -32,10 +52,11 @@ public class PlayerController : MonoBehaviour {
 
 			transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
 		}
-		
+
 	}
 
 	void FixedUpdate(){
 		rb.velocity = moveVelocity;
 	}
+*/
 }
