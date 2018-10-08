d0067d6267bf611b543fb42769278aae8b977375
Ferran Pérez <ferranpf98@gmail.com>
2018-10-08 16:28:16 +0200

    PlayerController y FollowCamera

---
+++ Assets/Prototipo/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Transform PlayerTransform;
+
+	private Vector3 offset;
+
+	[Range(0.01f, 1.0f)]
+	public float Smooth = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 newPos = PlayerTransform.position + offset;
+
+		transform.position = Vector3.Slerp(transform.position, newPos, Smooth);
+	}
+}
---
+++ Assets/Prototipo/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Camera mainCamera;
+	private Rigidbody rb;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		mainCamera = FindObjectOfType<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput * moveSpeed;
+ 
+		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayLength;
+
+		if(groundPlane.Raycast(cameraRay, out rayLength)){
+			Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+			Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+			transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		}
+		
+	}
+
+	void FixedUpdate(){
+		rb.velocity = moveVelocity;
+	}
+}
