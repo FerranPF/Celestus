a994e0d20673bbaec63ea3ca1a978872d065a28c
Ferran Pérez <ferranpf98@gmail.com>
2018-10-08 17:25:19 +0200

    Añadimos modelo de pruebas y animaciones

--- Assets/Prototipo/Scripts/PlayerController.cs
+++ Assets/Prototipo/Scripts/PlayerController.cs
@@ -4,59 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	
+	private Rigidbody rb;
+	private Animator anim;
+
 	[SerializeField]
 	float movementSpeed = 0.0f;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
 	void Update(){
 		ControlPlayer();
 	}
 	void ControlPlayer(){
 		float moveHorizonat = Input.GetAxisRaw("Horizontal");
 		float moveVertical = Input.GetAxisRaw("Vertical");
-
+		
 		Vector3 movement = new Vector3(moveHorizonat, 0.0f, moveVertical);
-		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
 		if(movement != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
 		transform.Translate (movement*movementSpeed*Time.deltaTime, Space.World);
-	}
 
-/*
-	public float moveSpeed;
-	private Camera mainCamera;
-	private Rigidbody rb;
-
-	private Vector3 moveInput;
-	private Vector3 moveVelocity;
-
-	void Start(){
-		rb = GetComponent<Rigidbody>();
-		mainCamera = FindObjectOfType<Camera>();
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-
-		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-		moveVelocity = moveInput * moveSpeed;
-
-		
-
-		
-		Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-		float rayLength;
-
-		if(groundPlane.Raycast(cameraRay, out rayLength)){
-			Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-			Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-
-			transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		if(movement != Vector3.zero){
+			anim.SetBool("moving", true);
+		}else{
+			anim.SetBool("moving", false);
 		}
-
 	}
-
-	void FixedUpdate(){
-		rb.velocity = moveVelocity;
-	}
-*/
 }
