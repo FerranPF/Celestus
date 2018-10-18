8544c78ff6ee5ce87e006491d35b24b2c76bd824
FerranPF <ferranpf98@gmail.com>
2018-10-18 19:03:10 +0200

    ProtoMenu Scene and UI Menu

---
+++ Assets/Prototipo/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    // Use this for initialization
+    public void ExitButtons()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    public void OptionsButton() {
+		
+	}
+
+    public void CreditsButton()
+    {
+
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Prototipo");
+    }
+}
