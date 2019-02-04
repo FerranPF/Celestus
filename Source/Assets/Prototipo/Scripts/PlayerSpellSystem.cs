e7fc77c69b3621e1a5178afc6aecd3eb5b3f278b
FerranPF <ferranpf98@gmail.com>
2019-02-04 17:04:38 +0100

    Fire spell projector

--- Source/Assets/Prototipo/Scripts/PlayerSpellSystem.cs
+++ Source/Assets/Prototipo/Scripts/PlayerSpellSystem.cs
@@ -8,15 +8,18 @@ public class PlayerSpellSystem : MonoBehaviour
     bool setTarget = false;
 
     bool target01 = false;
+    bool target02 = false;
     float spell1Time;
     public float spell1Mana;
     public GameObject spell1Sprite;
+    public GameObject spell2Sprite;
     GameManager manager;
 
     private void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         spell1Sprite.SetActive(false);
+        spell2Sprite.SetActive(false);
     }
 
     private void Update()
@@ -26,6 +29,12 @@ public class PlayerSpellSystem : MonoBehaviour
             if (Input.GetKey(KeyCode.Alpha1))
             {
                 target01 = true;
+                target02 = false;
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                target02 = true;
+                target01 = false;
             }
         }
 
@@ -39,6 +48,11 @@ public class PlayerSpellSystem : MonoBehaviour
                 StartCoroutine(CastLightningSpell());
                 spell1Sprite.SetActive(false);
             }
+        }
+
+        if (target02)
+        {
+
         }
 
     }
@@ -59,14 +73,47 @@ public class PlayerSpellSystem : MonoBehaviour
         }
     }
 
+    void ShowSpell2()
+    {
+        spell2Sprite.SetActive(true);
+        spell1Sprite.SetActive(true);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, 1.3f, 0));
+        float rayLength;
+        Vector3 pointToLook;
+
+        if (groundPlane.Raycast(ray, out rayLength))
+        {
+            pointToLook = ray.GetPoint(rayLength);
+            pointToLook = new Vector3(pointToLook.x, 1.3f, pointToLook.z);
+            spell1Sprite.transform.position = pointToLook;
+        }
+    }
+
     void SettingTarget()
     {
         if (Input.GetMouseButton(0))
         {
+            manager.playerController.canAttack = false;
             GetTarget();
             setTarget = true;
             spell1Sprite.SetActive(false);
         }
+        if (Input.GetMouseButton(1))
+        {
+            ResetTarget();
+        }
+    }
+
+    void ResetTarget()
+    {
+        spell1Sprite.SetActive(false);
+        spell2Sprite.SetActive(false);
+        manager.playerController.canAttack = true;
+        canSpell = true;
+        target01 = false;
+        target02 = false;
+        setTarget = false;
     }
 
     IEnumerator CastLightningSpell()
@@ -75,9 +122,7 @@ public class PlayerSpellSystem : MonoBehaviour
         canSpell = false;
         yield return new WaitForSeconds(spell1Time);
         manager.playerController.anim.SetBool("attack", false);
-        setTarget = false;
-        target01 = false;
-        canSpell = true;
+        ResetTarget();
     }
 
     void GetTarget()
