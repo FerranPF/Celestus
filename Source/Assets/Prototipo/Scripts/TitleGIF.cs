05c018e3468350bc1cb2c6a7c330096bbc23d341
Ferran PF <ferranpf98@gmail.com>
2019-02-18 17:19:41 +0100

    Logo GIF / Menu Buttons

---
+++ Source/Assets/Prototipo/Scripts/TitleGIF.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleGIF : MonoBehaviour
+{
+
+    public Texture2D[] frames;
+    public int fps = 10;
+
+    void Update()
+    {
+        int index = (int)(Time.time * fps) % frames.Length;
+        GetComponent<RawImage>().texture = frames[index];
+    }
+}
