eded49b20050b1f939f7fde58b3879ebe2ba4b01
FerranPF <ferranpf98@gmail.com>
2019-03-01 19:03:20 +0100

    Area Damage

---
+++ Source/Assets/Prototipo/Scripts/AreaDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaDamage : MonoBehaviour
+{
+    public float expandSpeed;
+    public float damage;
+
+    private void Update()
+    {
+        this.transform.localScale += new Vector3(expandSpeed, expandSpeed, 0)*Time.deltaTime;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().TakeDamage(damage);
+        }
+    }
+}
