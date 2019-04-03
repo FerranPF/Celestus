2e2a5470636617c6d50cca7f8f825c3d2da26731
LlorencMoll <llorens.moll8@gmail.com>
2019-04-03 17:11:24 +0200

    Tween MainMenu

--- Source/Assets/Prototipo/Scripts/MenuTweens.cs
+++ Source/Assets/Prototipo/Scripts/MenuTweens.cs
@@ -8,33 +8,47 @@ public class MenuTweens : MonoBehaviour
     public RectTransform options;
     public RectTransform exit;
     public RectTransform optionpanel;
+    public GameObject par1;
+    public GameObject par2;
+    public GameObject Floor1;
+    public GameObject Floor2;
+    
     // Start is called before the first frame update
     void Start()
     {
         newgame.DOAnchorPosX(-2111, 1f).From();
         options.DOAnchorPosX(-2111, 1f).From();
         exit.DOAnchorPosX(-2111, 1f).From();
-        
+        par2.SetActive(true);
+        par1.SetActive(false);
+        Floor2.SetActive(false);
+        Floor1.SetActive(true);
     }
 
 
  public void OptionButtonPressed()
     {
-        newgame.DOAnchorPosX(-2111, 1f);
-        options.DOAnchorPosX(-2111, 1f);
-        exit.DOAnchorPosX(-2111, 1f);
+        par2.SetActive(false);
+        newgame.DOAnchorPosX(-2000, 1f);
+        options.DOAnchorPosX(-2000, 1f);
+        exit.DOAnchorPosX(-2500, 1f);
         optionpanel.DOAnchorPosX(-520, 1f).SetDelay(1f);
+        par1.SetActive(true);
+        Floor2.SetActive(true);
+        Floor1.SetActive(false);
+        
 
     }
     // Update is called once per frame
    public void ButtonBack()
     {
-        newgame.DOAnchorPosX(1, 1f);
-        options.DOAnchorPosX(1, 1f);
-        exit.DOAnchorPosX(1, 1f);
-        newgame.DOAnchorPosX(-2111, 1f);
-        options.DOAnchorPosX(-137, 1f);
-        exit.DOAnchorPosX(137, 1f);
-        optionpanel.DOAnchorPosX(137, 1f).SetDelay(1f);
+        par2.SetActive(true);
+        newgame.DOAnchorPosX(0, 1f).SetDelay(1f);
+        options.DOAnchorPosX(0, 1f).SetDelay(1f);
+        exit.DOAnchorPosX(0, 1f).SetDelay(1f);
+        optionpanel.DOAnchorPosX(-2111, 1f);
+        par1.SetActive(false);
+        Floor2.SetActive(false);
+        Floor1.SetActive(true);
     }
 }
