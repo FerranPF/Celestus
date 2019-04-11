46ffd44b13c5848b335bc4d9f64db7eb8f7b8767
FerranPF <FerranPF@users.noreply.github.com>
2019-04-11 19:04:14 +0200

    Lightning spell changes / Postprocessing

--- Source/Assets/Prototipo/Scripts/LightningSpell.cs
+++ Source/Assets/Prototipo/Scripts/LightningSpell.cs
@@ -7,12 +7,15 @@ public class LightningSpell : MonoBehaviour
     public ParticleSystem[] PS;
     private BoxCollider coll;
 
+    public bool m_Started;
+
     public float timeTravel;
     public float travelSpeed;
     private float contTime = 0.0f;
     public int damage;
     private bool active;
     private Vector3 speedVector;
+    private string lastEnemy = " ";
 
     private void Start()
     {
@@ -36,20 +39,47 @@ public class LightningSpell : MonoBehaviour
 
         this.transform.Translate(speedVector*Time.deltaTime, Space.Self);
 
+        MyCollision();
+
         if(contTime >= timeTravel)
         {
             Destroy(this.gameObject);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void MyCollision()
     {
-        Debug.Log(other.gameObject.tag);
-        if (other.gameObject.tag == "Enemy")
+        Collider[] enemyColliders = Physics.OverlapBox(transform.position, transform.localScale, Quaternion.LookRotation(Vector3.forward, Vector3.up));
+        int i = 0;
+
+        //Check when there is a new collider coming into contact with the box
+        while (i < enemyColliders.Length)
         {
-            EnemyController enemy;
-            enemy = other.gameObject.GetComponent<EnemyController>();
-            enemy.GetDamage(damage);
+            //Damage every enemy in the collider
+            //Debug.Log("Hit : " + enemyColliders[i].name + i);
+            if (enemyColliders[i].tag == "Enemy")
+            {
+                Debug.Log(enemyColliders[i].gameObject.name);
+
+                if(enemyColliders[i].gameObject.name != lastEnemy)
+                {
+                    EnemyController enemy = enemyColliders[i].GetComponent<EnemyController>();
+                    lastEnemy = enemyColliders[i].gameObject.name;
+                    enemy.GetDamage(damage);
+                }
+            }
+
+            //Increase the number of Colliders in the array
+            i++;
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
+        if (m_Started)
+            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+            Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
