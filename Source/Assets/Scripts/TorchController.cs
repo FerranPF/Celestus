df3b6f29b78c3c9855dcc2bf742fdfefdb0824cb
FerranPF <FerranPF@users.noreply.github.com>
2019-05-07 19:02:34 +0200

    Torch light optimization

---
+++ Source/Assets/Scripts/TorchController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    private Light torchLight;
+    private float baseIntensity;
+    public float randomTime;
+    private void Awake()
+    {
+        torchLight = GetComponentInChildren<Light>();
+        baseIntensity = torchLight.intensity;
+        //Invoke("RandomLight", randomTime);
+    }
+
+    private void Update()
+    {
+        //InvokeRepeating("RandomLight", Time.deltaTime, randomTime);
+        //Invoke("RandomLight", randomTime);
+        StartCoroutine(RandomLight());
+    }
+
+    private IEnumerator RandomLight()
+    {
+        //torchLight.color = new Color(1-(Random.Range(0.8f, 1)), 0, 0, 1);
+        yield return new WaitForSecondsRealtime(randomTime);
+        torchLight.intensity = baseIntensity - Random.Range(2f, 5f);
+    }
+}
