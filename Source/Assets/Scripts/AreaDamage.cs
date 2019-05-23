e53e5fbf4bff887703627bf2295f7cb0bf6eca04
FerranPF <FerranPF@users.noreply.github.com>
2019-05-23 19:09:22 +0200

    Father particle system

--- Source/Assets/Scripts/AreaDamage.cs
+++ Source/Assets/Scripts/AreaDamage.cs
@@ -8,10 +8,19 @@ public class AreaDamage : MonoBehaviour
     public float damage;
     private PlayerController player;
     public GameObject particles;
+    private float cont;
+    public float timeToDestroy = 5.0f;
 
     private void Update()
     {
         this.transform.localScale += new Vector3(expandSpeed, expandSpeed, 0)*Time.deltaTime;
+
+        cont += Time.deltaTime;
+        if(cont >= timeToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
 
